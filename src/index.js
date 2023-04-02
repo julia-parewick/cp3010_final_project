9f964f6772a66695560542615eb7e1f9026902a6
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2023-04-02 15:19:25 -0230

    login and logout with Auth0/OAuth

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </BrowserRouter>
 );
