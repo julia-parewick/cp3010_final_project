0391a97297689f9e9a4ea676b7a644016901c095
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2023-04-13 18:19:54 -0230

    clean up

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { Auth0ProviderWithNavigate } from './auth0-provider-with-navigate';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>
 );
