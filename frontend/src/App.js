9edc6aa13574517c8f7eb344193ec2521e2b2266
Hill-Farrell <hillary.farrell85@ed.cna.nl.ca>
2023-04-16 13:54:45 -0230

    btns react to right/wrong anwers by changing color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+//import './App.css';
 import { Header } from './components/header';
 import { Greeting } from './components/greeting';
 import { useEffect, useState } from 'react';
@@ -13,76 +13,97 @@ import Col from 'react-bootstrap/Col';
 let index = 0;
 function TriviaGame(props) {
   const { isAuthenticated } = useAuth0();
-  console.log(isAuthenticated);
+  //console.log(isAuthenticated);
   let [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [buttonVariants, setButtonVariants] = useState(['info','info','info','info']);
   let [index,setIndex] = useState(0);
 
-  // function handleAnswerClick(answer) {
-  //   setSelectedAnswer(answer);
-  //   index++;
-  //   console.log(selectedAnswer)
-  // }
-
-  //console.log(props.questions)
   props.questions.map((q,i)=>{
     q.answers = q.incorrect_answers;
     q.answers.push(q.correct_answer);
   }) 
 
-  const handleAnswerClick = (questionObject, selectedAnswer) => {
+  const handleAnswerClick = (questionObject, selectedAnswer, btnID) => {
     if(props.questions.indexOf(questionObject)==0){
       localStorage.setItem("score",0);
       if(localStorage.getItem("streak")==null){
         localStorage.setItem("streak",0);
       }
       if(questionObject.correct_answer == selectedAnswer){
+        btnColorCorrect(btnID)
         localStorage.score++;
         localStorage.streak++;
       }else{
+        btnColorIncorrect(btnID)
         localStorage.setItem("streak",0);
       }
-      setIndex(index+1);
+      setTimeout( () => {
+        setButtonVariants(['info','info','info','info']);
+        setIndex(index+1);
+      }, 1000);
+      //setIndex(index+1);
     }
     else if(props.questions.indexOf(questionObject)==9){
       if(questionObject.correct_answer == selectedAnswer){
+        btnColorCorrect(btnID)
         localStorage.score++;
         localStorage.streak++;
       }else{
+        btnColorIncorrect(btnID)
         localStorage.setItem("streak",0);
       }
       document.getElementById("quiz").innerHTML=`<h1>THANK YOU FOR PLAYING<h1/>`
     }
     else{
       if(questionObject.correct_answer == selectedAnswer){
+        btnColorCorrect(btnID)
         localStorage.score++;
         localStorage.streak++;
       }else{
+        btnColorIncorrect(btnID)
         localStorage.setItem("streak",0);
       }
-      setIndex(index+1);
+
+      setTimeout( () => {
+        setButtonVariants(['info','info','info','info']);
+        setIndex(index+1);
+      }, 1000);
     }
-    console.log(selectedAnswer)
+    //console.log(selectedAnswer)
   };
+
+  function btnColorCorrect(index){
+    const newButtonVariants = [...buttonVariants]
+    newButtonVariants[index] = 'success';
+    setButtonVariants(newButtonVariants);
+  }
+
+  function btnColorIncorrect(index){
+    const newButtonVariants = [...buttonVariants]
+    newButtonVariants[index] = 'danger';
+    setButtonVariants(newButtonVariants);
+  }
 
   return (
     isAuthenticated &&(<div>
         <div key={index}>
-          <Row>
-            <Col>
-              <h2>{props.questions[index].question}</h2>
-              <Button style={{ width: "200px", height: "75px"}}
-              onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[0])}}>{props.questions[index].answers[0]}</Button>
-              <Button style={{ width: "200px", height: "75px",}}
-              onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[1])}}>{props.questions[index].answers[1]}</Button>
-              <Button style={{ width: "200px", height: "75px",}}
-              onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[2])}}>{props.questions[index].answers[2]}</Button>
-              <Button style={{ width: "200px", height: "75px",}}
-              onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[3])}}>{props.questions[index].answers[3]}</Button><br></br>
-            </Col>
-          </Row>
-
+            <Row>
+              <Col>
+                <div>
+                <br /><h2>Score: {localStorage.score}</h2><br />
+                <h2>{props.questions[index].question}</h2>
+                <Button variant={buttonVariants[0]} style={{ width: "200px", height: "75px",}}
+                onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[0], 0)}}>{props.questions[index].answers[0]}</Button>
+                <Button variant={buttonVariants[1]} style={{ width: "200px", height: "75px",}}
+                onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[1], 1)}}>{props.questions[index].answers[1]}</Button>
+                <Button variant={buttonVariants[2]} style={{ width: "200px", height: "75px",}}
+                onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[2], 2)}}>{props.questions[index].answers[2]}</Button>
+                <Button variant={buttonVariants[3]} style={{ width: "200px", height: "75px",}}
+                onClick={()=>{handleAnswerClick(props.questions[index],props.questions[index].answers[3], 3)}}>{props.questions[index].answers[3]}</Button><br /><br />
+                </div>
+              </Col>
+            </Row>
         </div>
-      {/* <p>You selected: {selectedAnswer}</p> */}
     </div>)
   );
 }
@@ -97,8 +118,6 @@ function Index(){
       .then(setQuestions)
       .catch(e=>console.log(e.message));
   }, []);
-
-  console.log(questions);
 
   return(
     <>
